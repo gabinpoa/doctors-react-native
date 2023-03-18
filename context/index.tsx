91c8f9de54797246e0e9907a46edd260edfc1620
Gabriel Nascimento <gabrielnpoa@gmail.com>
2023-03-18 15:33:56 -0300

    let you fetch the start and end hours from db

--- context/index.tsx
+++ context/index.tsx
@@ -2,6 +2,11 @@ import { createContext, useState } from "react";
 import * as React from "react";
 import { IDataToCreate, TCalendar } from "../types";
 
+export interface LimitHours {
+  start: number;
+  end: number;
+}
+
 export interface IContextDefaultValue {
   logged: boolean;
   setLogged: React.Dispatch<React.SetStateAction<boolean>>;
@@ -11,6 +16,8 @@ export interface IContextDefaultValue {
   >;
   calendar: TCalendar | [];
   setCalendar: React.Dispatch<React.SetStateAction<TCalendar | []>>;
+  limitHours: LimitHours;
+  setLimitHours: React.Dispatch<React.SetStateAction<LimitHours>>;
 }
 
 export const AppContext = createContext<IContextDefaultValue | null>(null);
@@ -19,6 +26,10 @@ export const AppContextProvider = ({ children }: React.PropsWithChildren) => {
   const [dataToCreate, setDataToCreate] = useState<IDataToCreate>();
   const [calendar, setCalendar] = useState<TCalendar | []>([]);
   const [logged, setLogged] = useState(false);
+  const [limitHours, setLimitHours] = useState<LimitHours>({
+    start: 6,
+    end: 23,
+  });
 
   return (
     <AppContext.Provider
@@ -29,6 +40,8 @@ export const AppContextProvider = ({ children }: React.PropsWithChildren) => {
         setDataToCreate,
         calendar,
         setCalendar,
+        setLimitHours,
+        limitHours,
       }}
     >
       {children}
--- hooks/getHoursColumn.ts
+++ hooks/getHoursColumn.ts
@@ -1,18 +1,15 @@
+import { LimitHours } from "../context";
 import getFinalDate from "./getFinalDate";
 import getInitialDate from "./getInitialDate";
 
-function getHoursColumn(
-  date: Date,
-  limitHour: { initial: number; final: number }
-) {
-  const initialDate = getInitialDate(date, limitHour.initial);
-  const finalDate = getFinalDate(date, limitHour.final);
+function getHoursColumn(date: Date, limitHours: LimitHours) {
+  const initialDate = getInitialDate(date, limitHours.start);
+  const finalDate = getFinalDate(date, limitHours.end);
 
   const hoursColumn = [];
   for (
     let counterDate = new Date(initialDate);
-    counterDate.getHours() <= finalDate.getHours() &&
-    counterDate.getHours() !== 0;
+    counterDate <= finalDate;
     counterDate.setHours(counterDate.getHours() + 1)
   ) {
     const row = {
